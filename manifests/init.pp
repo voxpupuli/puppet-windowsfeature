30b26316d02fdb7d3332abd288b8e5098b830fdf
Stephen Hoekstra <stephenhoekstra@gmail.com>
2014-04-03 23:10:02 +0200

    * removed duplicated "when removing a windows feature" test * changed $restart to be a boolean, added relevant tests * used validate_re function for $ensure, removed test as no longer required * neatened up remaining tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,9 @@
-define windowsfeature($feature_name = $title, $ensure = 'present', $restart = 'false') {
+define windowsfeature($feature_name = $title, $ensure = 'present', $restart = false) {
 
-  if ! ($ensure in [ 'present', 'absent' ]) {
-    fail('valid values for ensure are \'present\' or \'absent\'')
-  }
+  validate_re($ensure, '^(present|absent)$', 'valid values for ensure are \'present\' or \'absent\'')
+  validate_bool($restart)
 
-  validate_re($restart, '^(false|true)$', 'restart must be one of \'true\' or \'false\'')
+  if $restart { $_restart = 'true' } else { $_restart = 'false' }
 
   if(is_array($feature_name)){
     $escaped = join(prefix(suffix($feature_name,"'"),"'"),',')
@@ -15,13 +14,13 @@ define windowsfeature($feature_name = $title, $ensure = 'present', $restart = 'f
 
   if ($ensure == 'present') {
     exec { "add-feature-${title}" :
-      command   => "Import-Module ServerManager; Add-WindowsFeature ${features} -Restart:$${restart}",
+      command   => "Import-Module ServerManager; Add-WindowsFeature ${features} -Restart:$${_restart}",
       onlyif    => "Import-Module ServerManager; if((Get-WindowsFeature ${features} | where InstallState -eq 'Available').count -eq 0){ exit 1 }",
       provider  => powershell
     }
   } elsif ($ensure == 'absent') {
     exec { "remove-feature-${title}" :
-      command   => "Import-Module ServerManager; Remove-WindowsFeature ${$features} -Restart:$${restart}",
+      command   => "Import-Module ServerManager; Remove-WindowsFeature ${$features} -Restart:$${_restart}",
       unless    => "Import-Module ServerManager; if((Get-WindowsFeature ${features} | where InstallState -eq 'Installed').count -gt 0){ exit 1 }",
       provider  => powershell
     }
