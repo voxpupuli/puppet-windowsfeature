db8763225a70c4c4c47daba3b028b4c346456659
liamjbennett <lbennett@opentable.com>
2014-06-05 11:19:09 +0100

    fixing tests on puppet 3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'stdlib', 'lib')
+$:.unshift File.join(File.dirname(__FILE__),  'fixtures', 'modules', 'powershell', 'lib')
