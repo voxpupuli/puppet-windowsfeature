dab39a64a676bb3c9d891c075770536bada84bd1
Peter Souter <p.morsou@gmail.com>
2016-05-24 23:26:09 +0100

    Update acceptance tests
    * Adds ensure
    * Adds an acceptance test for lots of features

---
+++ spec/acceptance/lots_of_features_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper_acceptance'
+
+describe 'windowsfeature' do
+  context 'lots of features at once' do
+    it 'should be installed and not take too long' do
+      pp = <<-PP
+        $windowsfeatures = [
+          'Web-Filtering',
+          'Web-Http-Errors',
+          'Web-Net-Ext',
+          'Web-Mgmt-Tools',
+          'Web-ISAPI-Ext',
+          'Web-Default-Doc',
+          'Web-Stat-Compression',
+          'Web-Http-Logging',
+          'Web-Mgmt-Console',
+          'Web-ISAPI-Filter',
+          'Web-Static-Content',
+          'Web-Request-Monitor',
+          'Web-Dyn-Compression',
+        ]
+
+        windowsfeature { $windowsfeatures:
+          ensure => present,
+        }
+      PP
+
+      apply_manifest(pp, catch_failures: true)
+      expect(apply_manifest(pp, catch_failures: true).exit_code).to be_zero
+    end
+  end
+end
