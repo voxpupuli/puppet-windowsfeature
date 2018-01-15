f638d200098a3fa1d8d368e40ffe2a80ee0cae96
tragiccode <6224270+TraGicCode@users.noreply.github.com>
2018-01-15 08:16:57 -0600

    Fixed rubocop violations.

--- spec/acceptance/windowsfeature_spec.rb
+++ spec/acceptance/windowsfeature_spec.rb
@@ -2,22 +2,21 @@ require 'spec_helper_acceptance'
 
 describe 'windowsfeature' do
   context 'when managing a windows feature' do
-
-      let(:manifest) do
-        <<-MANIFEST
+    let(:manifest) do
+      <<-MANIFEST
             windowsfeature { 'Web-Http-Redirect':
               ensure => present,
             }
           MANIFEST
-      end
+    end
 
-      it 'runs without errors' do
-        apply_manifest(manifest, catch_failures: true)
-      end
+    it 'runs without errors' do
+      apply_manifest(manifest, catch_failures: true)
+    end
 
-      it 'runs a second time without changes' do
-        apply_manifest(manifest, catch_changes: true)
-      end
+    it 'runs a second time without changes' do
+      apply_manifest(manifest, catch_changes: true)
+    end
   end
 
   describe windows_feature('Web-Http-Redirect') do
